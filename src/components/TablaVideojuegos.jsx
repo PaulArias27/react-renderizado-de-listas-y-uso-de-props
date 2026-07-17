61f6818ad315e3ad39e4400ee32fabda88e88ae0
PaulArias27 <paularias2727@gmail.com>
2026-07-16 23:18:24 -0500

    feat:PA: se modigico el app.jsx

--- src/components/TablaVideojuegos.jsx
+++ src/components/TablaVideojuegos.jsx
@@ -6,6 +6,7 @@ function TablaVideojuegos({ videojuegos }) {
       <table>
         <thead>
           <tr>
+            <th>Portada</th>
             <th>Título</th>
             <th>Género</th>
             <th>Plataforma</th>
@@ -19,16 +20,27 @@ function TablaVideojuegos({ videojuegos }) {
         <tbody>
           {videojuegos.map((juego) => (
             <tr key={juego.id}>
+
+              <td>
+                <img
+                  src={juego.imagen}
+                  alt={juego.titulo}
+                  className="portada"
+                />
+              </td>
+
               <td>{juego.titulo}</td>
               <td>{juego.genero}</td>
               <td>{juego.plataforma}</td>
               <td>{juego.lanzamiento}</td>
               <td>${juego.precio}</td>
               <td>{juego.disponible ? "Sí" : "No"}</td>
+
               <td>
                 <progress value={juego.progreso} max="1"></progress>
                 <span> {Math.round(juego.progreso * 100)}%</span>
               </td>
+
             </tr>
           ))}
         </tbody>
