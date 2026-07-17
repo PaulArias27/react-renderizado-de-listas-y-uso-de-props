7418387920cacf5663f3ad94c5b94b7513fee6e0
PaulArias27 <paularias2727@gmail.com>
2026-07-16 23:00:46 -0500

    feat:PA: se inicio el taller, se agrego la data y el comoponent

---
+++ src/data/videojuegos.js
@@ -0,0 +1,59 @@
+const videojuegos = [
+  {
+    id: 1,
+    titulo: "The Legend of Zelda: Tears of the Kingdom",
+    genero: "Aventura",
+    plataforma: "Nintendo Switch",
+    lanzamiento: 2023,
+    precio: 59.99,
+    disponible: true,
+    progreso: 0.85,
+    imagen: "https://upload.wikimedia.org/wikipedia/en/f/fb/The_Legend_of_Zelda_Tears_of_the_Kingdom_cover.jpg"
+  },
+  {
+    id: 2,
+    titulo: "EA Sports FC 25",
+    genero: "Deportes",
+    plataforma: "PC",
+    lanzamiento: 2024,
+    precio: 49.99,
+    disponible: true,
+    progreso: 0.65,
+    imagen: "https://upload.wikimedia.org/wikipedia/en/3/37/EA_Sports_FC_25_cover.jpg"
+  },
+  {
+    id: 3,
+    titulo: "Minecraft",
+    genero: "Sandbox",
+    plataforma: "Multiplataforma",
+    lanzamiento: 2011,
+    precio: 29.99,
+    disponible: true,
+    progreso: 1,
+    imagen: "https://upload.wikimedia.org/wikipedia/en/5/51/Minecraft_cover.png"
+  },
+  {
+    id: 4,
+    titulo: "Cyberpunk 2077",
+    genero: "RPG",
+    plataforma: "PS5",
+    lanzamiento: 2020,
+    precio: 39.99,
+    disponible: false,
+    progreso: 0.25,
+    imagen: "https://upload.wikimedia.org/wikipedia/en/9/9f/Cyberpunk_2077_box_art.jpg"
+  },
+  {
+    id: 5,
+    titulo: "Grand Theft Auto V",
+    genero: "Acción",
+    plataforma: "Xbox Series X",
+    lanzamiento: 2013,
+    precio: 34.99,
+    disponible: true,
+    progreso: 0.95,
+    imagen: "https://upload.wikimedia.org/wikipedia/en/a/a5/Grand_Theft_Auto_V.png"
+  }
+];
+
+export default videojuegos;
